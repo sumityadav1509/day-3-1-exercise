5a73593ff829b3e210b054efcc86170f65353849
sumityadav1509 <sumityadav2005@gmail.com>
2020-12-09 18:17:18 +0000

    Checking a number if it is even or odd exercise completed.

--- main.py
+++ main.py
@@ -3,7 +3,11 @@ number = int(input("Which number do you want to check? "))
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
+even_number=number%2
 
-
+if even_number==0:
+  print("The number is Even")
+else:
+  print("Number is odd")
 
 
